ab6d17e63a938bbfccd590777104542592281d86
Avs <snehithanuka2006@gmail.com>
2025-01-07 19:43:43 +0530

    first commit

---
+++ src/day5.java
@@ -0,0 +1,18 @@
+/**public class day5 {
+    public static void main(String[] args) {
+        for(int i=20;i<=35;i++){
+            System.out.println(i);
+        }
+    }
+}
+**/
+public class day5{
+    public static void main(String[] args) {
+        
+        for (int i = 11; i <= 19; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+    }
+}
+}
